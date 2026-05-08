adda1b8411ff5543e9f8ae2fa23802bf18f67f4b
angela860807 <angela860@naver.com>
2026-05-08 12:35:59 +0900

    fix: trafficAS-b 폴더 삭제

--- trafficAS-b/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <RouterView />
-  <AuthModal />
-</template>
-
-<script setup>
-import { RouterView } from 'vue-router'
-import AuthModal from '@/components/AuthModal.vue'
-</script>
--- trafficAS-b/src/composables/useTheme.js
+++
@@ -1,6 +0,0 @@
-import { ref } from 'vue'
-const isDark = ref(true)
-export function useTheme() {
-  const toggle = () => { isDark.value = !isDark.value }
-  return { isDark, toggle }
-}
